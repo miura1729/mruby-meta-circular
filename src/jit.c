58f9222723fcad994e27daabd0b141986686d320
Miura Hideki <m-72@tf6.so-net.ne.jp>
2013-01-01 11:16:34 +0900

    Bugfix of previous bugfix

--- src/jit.c
+++ src/jit.c
@@ -90,10 +90,10 @@ mrbjit_dispatch(mrb_state *mrb, mrb_irep *irep, mrb_code **ppc, mrb_value *regs)
   mrbjit_code_area cbase;
   mrb_code *prev_pc;
 
- retry:
+  prev_pc = irep->compile_info->prev_pc;
+
   cbase = irep->compile_info->code_base;
   n = ISEQ_OFFSET_OF(*ppc);
-  prev_pc = irep->compile_info->prev_pc;
   if (prev_pc) {
     ci = search_codeinfo_prev(irep->jit_entry_tab + n, prev_pc);
   }
@@ -132,9 +132,6 @@ mrbjit_dispatch(mrb_state *mrb, mrb_irep *irep, mrb_code **ppc, mrb_value *regs)
       asm("pop %ebx");
       asm("pop %ecx");
       //printf("%x \n", *ppc);
-
-      /* Updated pc, so dispatch try again */
-      goto retry;
     }
   }
   else {
