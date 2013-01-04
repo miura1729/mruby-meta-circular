022d26b165dbaf16bced1d854a957e13bdb88a8b
Miura Hideki <m-72@tf6.so-net.ne.jp>
2013-01-04 20:48:50 +0900

    Add OP_SENDB

--- src/jitcode.cc
+++ src/jitcode.cc
@@ -69,6 +69,7 @@ mrbjit_emit_code(mrb_state *mrb, mrbjit_vmstatus *status)
   case OP_GETCONST:
     return code->emit_getconst(mrb, irep, ppc);
 
+  case OP_SENDB:
   case OP_SEND:
     return code->emit_send(mrb, status);
 
