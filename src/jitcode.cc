13dfa5db86dd5d4b40f8f9d81951b65d46df8c4c
Miura Hideki <m-72@tf6.so-net.ne.jp>
2013-03-17 17:17:57 +0900

    Modify for pass coi for assign local variables on registor

--- src/jitcode.cc
+++ src/jitcode.cc
@@ -39,9 +39,9 @@ mrbjit_gen_jmp_patch(mrbjit_code_area coderaw, void *dst, void *target)
 }
 
 static const void *
-mrbjit_emit_code_aux(mrb_state *mrb, mrbjit_vmstatus *status, MRBJitCode *code)
+mrbjit_emit_code_aux(mrb_state *mrb, mrbjit_vmstatus *status, 
+		     MRBJitCode *code, mrbjit_code_info *coi)
 {
-  mrb_irep *irep = *status->irep;
   mrb_value *regs = *status->regs;
   mrb_code **ppc = status->pc;
   const void *entry;
@@ -49,50 +49,50 @@ mrbjit_emit_code_aux(mrb_state *mrb, mrbjit_vmstatus *status, MRBJitCode *code)
   if (code == NULL) {
     code = the_code;
     mrb->compile_info.code_base = code;
-    entry = code->gen_entry(mrb, irep);
+    entry = code->gen_entry(mrb, status);
   }
 
   switch(GET_OPCODE(**ppc)) {
   case OP_NOP:
-    return code->emit_nop(mrb, irep, ppc);
+    return code->emit_nop(mrb, status, coi);
     
   case OP_MOVE:
-    return code->emit_move(mrb, irep, ppc);
+    return code->emit_move(mrb, status, coi);
 
   case OP_LOADL:
-    return code->emit_loadl(mrb, irep, ppc);
+    return code->emit_loadl(mrb, status, coi);
 
   case OP_LOADI:
-    return code->emit_loadi(mrb, irep, ppc);
+    return code->emit_loadi(mrb, status, coi);
 
     // OP_LOADSYM
 
   case OP_LOADSELF:
-    return code->emit_loadself(mrb, irep, ppc);
+    return code->emit_loadself(mrb, status, coi);
 
   case OP_LOADNIL:
-    return code->emit_loadnil(mrb, irep, ppc);
+    return code->emit_loadnil(mrb, status, coi);
 
   case OP_LOADT:
-    return code->emit_loadt(mrb, irep, ppc);
+    return code->emit_loadt(mrb, status, coi);
 
   case OP_LOADF:
-    return code->emit_loadf(mrb, irep, ppc);
+    return code->emit_loadf(mrb, status, coi);
 
   case OP_GETIV:
-    return code->emit_getiv(mrb, irep, ppc);
+    return code->emit_getiv(mrb, status, coi);
 
   case OP_SETIV:
-    return code->emit_setiv(mrb, irep, ppc);
+    return code->emit_setiv(mrb, status, coi);
 
   case OP_GETCV:
-    return code->emit_getcv(mrb, irep, ppc);
+    return code->emit_getcv(mrb, status, coi);
 
   case OP_SETCV:
-    return code->emit_setcv(mrb, irep, ppc);
+    return code->emit_setcv(mrb, status, coi);
 
   case OP_GETCONST:
-    return code->emit_getconst(mrb, irep, ppc);
+    return code->emit_getconst(mrb, status, coi);
 
   case OP_SENDB:
   case OP_SEND:
@@ -112,52 +112,52 @@ mrbjit_emit_code_aux(mrb_state *mrb, mrbjit_vmstatus *status, MRBJitCode *code)
     }
 
   case OP_ADD:
-    return code->emit_add(mrb, irep, ppc, regs);
+    return code->emit_add(mrb, status, coi, regs);
 
   case OP_SUB:
-    return code->emit_sub(mrb, irep, ppc, regs);
+    return code->emit_sub(mrb, status, coi, regs);
 
   case OP_MUL:
-    return code->emit_mul(mrb, irep, ppc, regs);
+    return code->emit_mul(mrb, status, coi, regs);
 
   case OP_DIV:
-    return code->emit_div(mrb, irep, ppc, regs);
+    return code->emit_div(mrb, status, coi, regs);
 
   case OP_ADDI:
-    return code->emit_addi(mrb, irep, ppc, regs);
+    return code->emit_addi(mrb, status, coi, regs);
 
   case OP_SUBI:
-    return code->emit_subi(mrb, irep, ppc, regs);
+    return code->emit_subi(mrb, status, coi, regs);
 
   case OP_EQ:
-    return code->emit_eq(mrb, irep, ppc, regs);
+    return code->emit_eq(mrb, status, coi, regs);
 
   case OP_LT:
-    return code->emit_lt(mrb, irep, ppc, regs);
+    return code->emit_lt(mrb, status, coi, regs);
 
   case OP_LE:
-    return code->emit_le(mrb, irep, ppc, regs);
+    return code->emit_le(mrb, status, coi, regs);
 
   case OP_GT:
-    return code->emit_gt(mrb, irep, ppc, regs);
+    return code->emit_gt(mrb, status, coi, regs);
 
   case OP_GE:
-    return code->emit_ge(mrb, irep, ppc, regs);
+    return code->emit_ge(mrb, status, coi, regs);
 
   case OP_GETUPVAR:
-    return code->emit_getupvar(mrb, irep, ppc);
+    return code->emit_getupvar(mrb, status, coi);
 
   case OP_SETUPVAR:
-    return code->emit_setupvar(mrb, irep, ppc);
+    return code->emit_setupvar(mrb, status, coi);
 
   case OP_JMP:
-    return code->emit_jmp(mrb, irep, ppc);
+    return code->emit_jmp(mrb, status, coi);
 
   case OP_JMPIF:
-    return code->emit_jmpif(mrb, irep, ppc, regs);
+    return code->emit_jmpif(mrb, status, coi, regs);
 
   case OP_JMPNOT:
-    return code->emit_jmpnot(mrb, irep, ppc, regs);
+    return code->emit_jmpnot(mrb, status, coi, regs);
 
   default:
     mrb->compile_info.nest_level = 0;
@@ -166,10 +166,10 @@ mrbjit_emit_code_aux(mrb_state *mrb, mrbjit_vmstatus *status, MRBJitCode *code)
 }
 
 const void *
-mrbjit_emit_code(mrb_state *mrb, mrbjit_vmstatus *status)
+mrbjit_emit_code(mrb_state *mrb, mrbjit_vmstatus *status, mrbjit_code_info *coi)
 {
   MRBJitCode *code = (MRBJitCode *)mrb->compile_info.code_base;
-  const void *rc = mrbjit_emit_code_aux(mrb, status, code);
+  const void *rc = mrbjit_emit_code_aux(mrb, status, code, coi);
   if (rc == NULL && code == NULL) {
     mrb->compile_info.code_base = NULL;
   }
