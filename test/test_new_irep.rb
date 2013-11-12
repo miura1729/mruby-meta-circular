e6c041203493fbc3f02806e96e9fb678de7cd24d
Miura Hideki <m-72@tf6.so-net.ne.jp>
2013-11-12 20:47:14 +0900

    Improve test_new_irep.rb

--- test/test_new_irep.rb
+++ test/test_new_irep.rb
@@ -3,8 +3,16 @@ include RiteOpcodeUtil
 iseq = [
   mkop_ABx(Irep::OPTABLE_CODE[:STRING], 1, 0),
   mkop_ABC(Irep::OPTABLE_CODE[:SEND], 0, 0, 1),
-  mk_opcode(Irep::OPTABLE_CODE[:RETURN])
+  mkop_ABx(Irep::OPTABLE_CODE[:NOP], 0, 1),
+  mkop_A(Irep::OPTABLE_CODE[:RETURN], 0)
 ]
 
-irep = Irep.new_irep(iseq, ["Hello World"], [:p], 2, 2)
-irep.to_proc.call
+irep = Irep.new_irep(iseq, ["Hello World", 0, 0], [:p], 2, 1)
+p = irep.to_proc
+
+i = 0
+while i < 100
+  p.call
+  i = i + 1
+end
+
