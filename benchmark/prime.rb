7e57f744b85e3a10437ce4b56e5cf559a48699c0
Miura Hideki <m-72@tf6.so-net.ne.jp>
2013-12-27 17:46:28 +0900

    Modify for inlining test

--- benchmark/prime.rb
+++ benchmark/prime.rb
@@ -1,3 +1,62 @@
+class Range
+
+  ##
+  # Calls the given block for each element of +self+
+  # and pass the respective element.
+  #
+  # ISO 15.2.14.4.4
+  def each(&block)
+    val = self.first
+    unless val.respond_to? :succ
+      raise TypeError, "can't iterate"
+    end
+
+    last = self.last
+    return self if (val <=> last) > 0
+
+    while((val <=> last) < 0)
+      block.call(val)
+      val = val.succ
+    end
+
+    if not exclude_end? and (val <=> last) == 0
+      block.call(val)
+    end
+    self
+  end
+end
+
+module Enumerable
+
+  ##
+  # Call the given block for each element
+  # which is yield by +each+. Return false
+  # if one block value is false. Otherwise
+  # return true. If no block is given and
+  # +self+ is false return false.
+  #
+  # ISO 15.3.2.2.1
+  def all?(&block)
+    st = true
+    if block
+      self.each{|val|
+        unless block.call(val)
+          st = false
+          break
+        end
+      }
+    else
+      self.each{|val|
+        unless val
+          st = false
+          break
+        end
+      }
+    end
+    st
+  end
+end
+
 def is_prime? n
   (2...n).all? { |i| n % i != 0 }
 end
