c9732ea616a896ca5cfcbed4efa63c448d70e914
Miura Hideki <m-72@tf6.so-net.ne.jp>
2014-04-18 21:26:11 +0900

    mruby config bat modified (I don't touch)

--- mrbgems/mruby-bin-mruby-config/mruby-config.bat
+++ mrbgems/mruby-bin-mruby-config/mruby-config.bat
@@ -6,7 +6,6 @@ if "%0" equ "" goto :eof
 if "%0" equ "--cflags" goto cflags
 if "%0" equ "--ldflags" goto ldflags
 if "%0" equ "--libs" goto libs
-if "%0" equ "--help" goto showhelp
 echo Invalid Option
 goto :eof
 
@@ -21,10 +20,3 @@ goto top
 :ldflags
 echo MRUBY_LDFLAGS
 goto top
-
-:showhelp
-echo Usage: mruby-config [switches]
-echo   switches:
-echo   --cflags    print flags passed to compiler
-echo   --ldflags   print flags passed to linker
-echo   --libs      print linked libraries
