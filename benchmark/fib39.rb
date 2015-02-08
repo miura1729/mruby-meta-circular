01dd34b8b62a9fb3c74e2c050eb562651d24b380
Miura Hideki <m-72@tf6.so-net.ne.jp>
2015-02-08 21:23:12 +0900

    Disp typeinfo for fib39

--- benchmark/fib39.rb
+++ benchmark/fib39.rb
@@ -5,12 +5,16 @@ def fib n
   fib(n-2) + fib(n-1)
 end
 
-puts fib(5.0)
 puts fib(39)
+
+puts fib(39.0)
 
 include RiteOpcodeUtil
 irep = Irep::get_irep(self, :fib)
 #irep = Irep::get_irep(Vec.new(-2.0, 0.0, -3.5), :vdot)
 irep.iseq.each_with_index {|ele, i|
-  print "#{Irep::disasm(ele, irep)} \t #{irep.reg_class(i)[0]}\n"
+  print "#{Irep::disasm(ele, irep)} \n"
+  irep.reg_type(i).each do |ele2|
+    print " \t #{ele2}\n"
+  end
 }
