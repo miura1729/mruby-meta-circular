5ae93f04acafcb3e1969f62a32928eaa637b78a4
Miura Hideki <m-72@tf6.so-net.ne.jp>
2015-06-15 18:52:05 +0900

    Divide Allocate Code to macro for reduce  common code

--- mrbgems/mruby-parray/src/prim.cc
+++ mrbgems/mruby-parray/src/prim.cc
@@ -8,6 +8,75 @@ extern "C" {
 #include "mruby/opcode.h"
 }
 
+#define PVEC4_ALLOCATE                                               \
+do {                                                                 \
+   /* TODO add guard of class  */                                    \
+                                                                     \
+  inLocalLabel();                                                    \
+   /* obj = mrbjit_instance_alloc(mrb, klass); */                    \
+  if (civoff >= 0) {                                                 \
+    emit_local_var_value_read(mrb, coi, reg_tmp0, a);                \
+    emit_move(mrb, coi, reg_tmp0, eax, OffsetOf(struct RObject, c)); \
+    emit_move(mrb, coi, reg_tmp0, eax, OffsetOf(struct RObject, iv)); \
+    emit_move(mrb, coi, reg_tmp0, reg_tmp0, 0);                      \
+    emit_push(mrb, coi, eax);			/* PUSH __objcache__ */ \
+    emit_move(mrb, coi, reg_tmp1, reg_tmp0, civoff * sizeof(mrb_value) + 4); \
+    emit_move(mrb, coi, reg_tmp0, reg_tmp0, civoff * sizeof(mrb_value)); \
+    test(eax, eax);                                                  \
+    jz(".empty");                                                    \
+    push(eax);                                                       \
+    push(edx);                                                       \
+    emit_move(mrb, coi, reg_tmp1, reg_tmp0, OffsetOf(struct RObject, c));  \
+    test(edx, edx);                                                  \
+    jz(".setnil");                                                   \
+    push(edx);                                                       \
+    emit_cfunc_start(mrb, coi);                                      \
+    emit_arg_push(mrb, coi, 1, reg_tmp0);                            \
+    emit_arg_push(mrb, coi, 0, esi);                                 \
+    call((void *)mrb_write_barrier);                                 \
+    emit_cfunc_end(mrb, coi, sizeof(mrb_state *) + sizeof(struct RBasic *)); \
+    pop(edx);                                                        \
+                                                                     \
+    emit_move(mrb, coi, reg_tmp0, esp, 8);                           \
+    emit_move(mrb, coi, reg_tmp0, civoff * sizeof(mrb_value), reg_tmp1); \
+    jmp(".ee");                                                      \
+    L(".setnil");                                                    \
+    emit_move(mrb, coi, reg_tmp0, esp, 8);                           \
+    emit_move(mrb, coi, reg_tmp0, civoff * sizeof(mrb_value), reg_tmp1); \
+    emit_load_literal(mrb, coi, reg_tmp1, 0xfff00000 | MRB_TT_FALSE); \
+    emit_move(mrb, coi, reg_tmp0, civoff * sizeof(mrb_value) + 4, reg_tmp1); \
+    L(".ee");                                                         \
+    pop(edx);                                                         \
+    pop(eax);                                                         \
+    jmp("@f");                                                        \
+    L(".empty");                                                      \
+  }                                                                   \
+                                                                      \
+  emit_cfunc_start(mrb, coi);                                         \
+  emit_load_literal(mrb, coi, reg_tmp0, 4);                           \
+  emit_arg_push(mrb, coi, 1, reg_tmp0);                               \
+  emit_arg_push(mrb, coi, 0, esi);                                    \
+  call((void *)mrb_ary_new_capa);                                     \
+  emit_cfunc_end(mrb, coi, 2 * sizeof(void *));                       \
+                                                                      \
+  L("@@");                                                            \
+ } while(0)
+
+#define PVEC4_ALLOCATE_FIN                                            \
+do {                                                                  \
+  if (civoff >= 0) {                                                  \
+    emit_pop(mrb, coi, eax);			/* POP __objcache__ */ \
+  }                                                                   \
+                                                                      \
+  emit_local_var_value_read(mrb, coi, reg_tmp0, a);                   \
+  emit_move(mrb, coi, reg_tmp0, OffsetOf(struct RArray, c), (Xbyak::uint32)c); \
+  outLocalLabel();                                                    \
+                                                                      \
+  dinfo->type = MRB_TT_ARRAY;                                         \
+  dinfo->klass = c;                                                   \
+  dinfo->constp = 0;                                                  \
+} while(0)
+
 mrb_value
 MRBJitCode::mrbjit_prim_pvec4_add_impl(mrb_state *mrb, mrb_value proc,
 				       mrbjit_vmstatus *status, mrbjit_code_info *coi)
@@ -27,56 +96,8 @@ MRBJitCode::mrbjit_prim_pvec4_add_impl(mrb_state *mrb, mrb_value proc,
 
   gen_flush_regs(mrb, pc, status, coi, 1);
 
-  // TODO add guard of class
-  
-  inLocalLabel();
-  // obj = mrbjit_instance_alloc(mrb, klass);
-  if (civoff >= 0) {
-    emit_local_var_value_read(mrb, coi, reg_tmp0, a);
-    emit_move(mrb, coi, reg_tmp0, eax, OffsetOf(struct RObject, c));
-    emit_move(mrb, coi, reg_tmp0, eax, OffsetOf(struct RObject, iv));
-    emit_move(mrb, coi, reg_tmp0, reg_tmp0, 0);
-    emit_push(mrb, coi, eax);			/* PUSH __objcache__ */
-    emit_move(mrb, coi, reg_tmp1, reg_tmp0, civoff * sizeof(mrb_value) + 4);
-    emit_move(mrb, coi, reg_tmp0, reg_tmp0, civoff * sizeof(mrb_value));
-    test(eax, eax);
-    jz(".empty");
-    push(eax);
-    push(edx);
-    emit_move(mrb, coi, reg_tmp1, reg_tmp0, OffsetOf(struct RObject, c));
-    test(edx, edx);
-    jz(".setnil");
-    push(edx);
-    emit_cfunc_start(mrb, coi);
-    emit_arg_push(mrb, coi, 1, reg_tmp0);
-    emit_arg_push(mrb, coi, 0, esi);
-    call((void *)mrb_write_barrier);
-    emit_cfunc_end(mrb, coi, sizeof(mrb_state *) + sizeof(struct RBasic *));
-    pop(edx);
+  PVEC4_ALLOCATE;
 
-    emit_move(mrb, coi, reg_tmp0, esp, 8);
-    emit_move(mrb, coi, reg_tmp0, civoff * sizeof(mrb_value), reg_tmp1);
-    jmp(".ee");
-    L(".setnil");
-    emit_move(mrb, coi, reg_tmp0, esp, 8);
-    emit_move(mrb, coi, reg_tmp0, civoff * sizeof(mrb_value), reg_tmp1);
-    emit_load_literal(mrb, coi, reg_tmp1, 0xfff00000 | MRB_TT_FALSE);
-    emit_move(mrb, coi, reg_tmp0, civoff * sizeof(mrb_value) + 4, reg_tmp1);
-    L(".ee");
-    pop(edx);
-    pop(eax);
-    jmp("@f");
-    L(".empty");
-  }    
-
-  emit_cfunc_start(mrb, coi);
-  emit_load_literal(mrb, coi, reg_tmp0, 4);
-  emit_arg_push(mrb, coi, 1, reg_tmp0);
-  emit_arg_push(mrb, coi, 0, esi);
-  call((void *)mrb_ary_new_capa);
-  emit_cfunc_end(mrb, coi, 2 * sizeof(void *));
-
-  L("@@");
   // regs[a] = obj;
   /* reg_tmp0 store pointer to new vector */
 
@@ -103,17 +124,8 @@ MRBJitCode::mrbjit_prim_pvec4_add_impl(mrb_state *mrb, mrb_value proc,
 
   emit_pop(mrb, coi, ebx);
 
-  if (civoff >= 0) {
-    emit_pop(mrb, coi, eax);			/* POP __objcache__ */ 
-  }    
+  PVEC4_ALLOCATE_FIN;
 
-  emit_local_var_value_read(mrb, coi, reg_tmp0, a);
-  emit_move(mrb, coi, reg_tmp0, OffsetOf(struct RArray, c), (Xbyak::uint32)c);
-  outLocalLabel();
-
-  dinfo->type = MRB_TT_ARRAY;
-  dinfo->klass = c;
-  dinfo->constp = 0;
 
   return mrb_true_value();
 }
