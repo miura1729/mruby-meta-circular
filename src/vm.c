f1e7fec4b1380178406f1c09a4613b53c27b5a19
Miura Hideki <m-72@tf6.so-net.ne.jp>
2015-09-22 17:04:42 +0900

    Bug fix for bm_app_fibbuzz

--- src/vm.c
+++ src/vm.c
@@ -1459,7 +1459,7 @@ mrbjit_dispatch(mrb_state *mrb, mrbjit_vmstatus *status)
 	ci->patch_pos = NULL;
       }
     }
-    else if (ci->used == 1) {
+    else if (ci->used == 1 && *ppc != irep->iseq) {
       if (prev_entry) {
 	cbase = mrb->compile_info.code_base;
 	mrbjit_gen_jmp_patch(mrb, cbase, prev_entry, ci->entry, status, ci);
