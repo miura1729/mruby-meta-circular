a493eec75802e8f937f28dfc48d6ac695ef6ac54
Miura Hideki <m-72@tf6.so-net.ne.jp>
2018-04-21 18:21:14 +0900

    Can execute make DAG

--- mrblib/saa.rb
+++ mrblib/saa.rb
@@ -1,20 +1,50 @@
 module RiteSSA
-  
-  class Reg
+  class Storable
     def initialize(ins)
       @genpoint = ins
       @type = nil
     end
-    
+  end
+
+  class Reg<Storable
+    def initialize(ins)
+      super
+    end
+
     attr_accessor :type
   end
-  
+
+  class SelfReg<Reg
+    def initialize(ins)
+      super
+      @ivlist = {}
+      @cvlist = {}
+    end
+
+    def get_iv(sym)
+      @ivlist[sym] || InstanceVariable.new(sym)
+    end
+
+    def get_cv(sym)
+      @cvlist[sym] || InstanceVariable.new(sym)
+    end
+  end
+
   class ParmReg<Reg
     def initialize(ins)
       super
     end
   end
-  
+
+  class InstanceVariable<Storable
+    def initialize(name)
+      @name = name
+      @type = nil
+    end
+
+    attr_accessor :type
+  end
+
   class Inst
     def initialize(op, irep)
       @op = op
@@ -23,13 +53,16 @@ module RiteSSA
       @outreg = []
       @para = []
     end
-    
-    attr_accessor :inreg
-    attr_accessor :outreg
+
+    attr :inreg
+    attr :outreg
   end
-  
+
   class RiteDAGNode
-    def initialize(irep, pos, iseq)
+    include RiteOpcodeUtil
+
+    def initialize(irep, pos, iseq, root)
+      @root = root
       @irep = irep
       @pos = pos
       @iseq = iseq
@@ -39,7 +72,7 @@ module RiteSSA
       @enter_reg = {}
       @exit_reg = {}
     end
-    
+
     attr :pos
     attr :iseq
     attr :ext_iseq
@@ -47,92 +80,236 @@ module RiteSSA
     attr :exit_link
     attr :enter_reg
     attr :exit_reg
-    
+
     def make_iseq_ext
-      res = []
-      regtab = []
-      @irep.nlocals.times do
-        regtab.push PramReg.new(@irep)
-      end
       @enter_reg = regtab.clone
-      
+      @ext_iseq = []
+
       @iseq.each do |code|
         op = get_opcode(code)
         inst = Inst.new(op, @irep)
         @ext_iseq.push inst
-        case OPTABLE[op]
+        case Irep::OPTABLE[op]
         when :NOP
-          
+
         when :MOVE
+          inst.inreg.push regtab[getarg_b(op)]
           dstreg = Reg.new(inst)
           regtab[getarg_a(op)] = dstreg
           inst.outreg.push dstreg
-          inst.inreg.push regtab[getarg_b(op)]
-          
+
         when :LOADL
-          dstreg = Reg.new
-          regtab[getarg_a(op)] = dstreg
-          inst.outreg.push dstreg
           inst.para.push @irep.pool[getarg_bx(op)]
-          
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
         when :LOADI
-          dstreg = Reg.new
-          regtab[getarg_a(op)] = dstreg
-          inst.outreg.push dstreg
           inst.para.push getarg_sbx(op)
-          
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
         when :LOADSYM
-          dstreg = Reg.new
-          regtab[getarg_a(op)] = dstreg
-          inst.outreg.push dstreg
           inst.para.push @irep.syms[getarg_bx(op)]
-          
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
         when :LOADNIL
-          dstreg = Reg.new
-          regtab[getarg_a(op)] = dstreg
-          inst.outreg.push dstreg
           inst.para.push nil
-          
-        when :LOADSELF
           dstreg = Reg.new
           regtab[getarg_a(op)] = dstreg
           inst.outreg.push dstreg
+
+        when :LOADSELF
           inst.inreg.push regtab[0]
-          
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
         when :LOADT
           dstreg = Reg.new
           regtab[getarg_a(op)] = dstreg
           inst.outreg.push dstreg
           inst.para.push true
-          
+
         when :LOADF
+          inst.para.push true
           dstreg = Reg.new
           regtab[getarg_a(op)] = dstreg
           inst.outreg.push dstreg
-          inst.para.push true
-          
+
         when :GETGLOBAL
+          inst.para.push @irep.syms[getarg_bx(op)]
           dstreg = Reg.new
           regtab[getarg_a(op)] = dstreg
-          inst.para.push @irep.syms[getarg_bx(op)]
-          
+
         when :SETGLOBAL
+          inst.para.push @irep.syms[getarg_bx(op)]
+          inst.inreg.push regtab[getarg_a(op)]
+
+        when :GETSPECIAL
+          inst.para.push @irep.syms[getarg_bx(op)]
           dstreg = Reg.new
           regtab[getarg_a(op)] = dstreg
+
+        when :SETSPECIAL
           inst.para.push @irep.syms[getarg_bx(op)]
-          
+          inst.inreg.push regtab[getarg_a(op)]
+
+        when :GETIV
+          name = @irep.syms[getarg_b(op)]
+          srcreg = regtab[0].get_iv(name)
+          inst.inreg.push srcreg
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
+        when :SETIV
+          inst.inreg.push regtab[getarg_b(op)]
+          name = @irep.syms[getarg_b(op)]
+          dstvar = regtab[0].get_iv(name)
+          inst.outreg.push dstvar
+
+        when :GETCV
+          name = @irep.syms[getarg_b(op)]
+          srcreg = regtab[0].get_cv(name)
+          inst.inreg.push srcreg
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
+        when :SETCV
+          inst.inreg.push regtab[getarg_b(op)]
+          name = @irep.syms[getarg_b(op)]
+          dstvar = regtab[0].get_cv(name)
+          inst.outreg.push dstvar
+
+        when :GETCONST
+          name = @irep.syms[getarg_b(op)]
+          src = @root.target_class.const_get(name)
+          inst.inreg.push val
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
+        when :SETCONST
+          name = @irep.syms[getarg_b(op)]
+          inst.inreg.push regtab[getarg_b(op)]
+          name = @irep.syms[getarg_b(op)]
+          dstreg = Reg.new
+          @root.target_class.set_constant(name, dstreg)
+          inst.outreg.push dstreg
+
+        when :GETMCONST
+          name = @irep.syms[getarg_b(op)]
+          src = @root.target_class.class.const_get(name)
+          inst.inreg.push val
+          dstreg = Reg.new
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
+        when :SETCONST
+          name = @irep.syms[getarg_b(op)]
+          inst.inreg.push regtab[getarg_b(op)]
+          name = @irep.syms[getarg_b(op)]
+          dstreg = Reg.new
+          @root.target_class.class.set_constant(name, dstreg)
+          inst.outreg.push dstreg
+
+        when :GETUPVAR
+          up = getarg_c(op)
+          pos = getarg_b(op)
+          curframe = @root
+          (up - 1).times do
+            curframe = curframe.parent
+          end
+          srcreg = curframe.regtab[pos]
+          inst.inreg.push srcreg
+          dstreg = Reg.new(inst)
+          regtab[getarg_a(op)] = dstreg
+          inst.outreg.push dstreg
+
+        when :SETUPVAR
+          up = getarg_c(op)
+          pos = getarg_b(op)
+          curframe = @root
+          (up - 1).times do
+            curframe = curframe.parent
+          end
+          dstreg = curframe.regtab[pos]
+          srcreg = regtab[getarg_a(op)]
+          inst.inreg.push srcreg
+          inst.outreg.push dstreg
+
+        when :JMP
+          inst.para.push getarg_sbx(op)
+
+        when :JMPIF, :JMPNOT
+          inst.inreg.pus regtab[getarg_a(op)]
+          inst.para.push getarg_sbx(op)
+
+        when :ONERR
+          inst.para.push getarg_sbx(op)
+
+        when :RESCUE
+          flg = getarg_c(op)
+          if flg == 1 then
+            # exception object compare mode
+
+          end
+
+        when :POPERR
+          inst.inreg.pus regtab[getarg_a(op)]
+
+        when :RAISE
+          inst.inreg.pus regtab[getarg_a(op)]
+
+        when :EPUSH
+          inst.para.push @irep.pool[getarg_bx(op)]
+
+        when :EPOP
+          inst.para.push @irep.pool[getarg_a(op)]
+
+        when :SEND, :SENDB
+          a = getarg_a(op)
+          name = @irep.syms[getarg_b(op)]
+          num = getarg_c(op)
+          inst.inreg.push regtab[a]
+          inst.para.push name
+          inst.para.push num
+          dstreg = Reg.new(inst)
+          regtab[a] = dstreg
+          inst.outreg.push dstreg # push self
+          inst.inreg.push dstreg # push self
+          if num == 127 then
+            inst.inreg.push regtab[a + 1];
+            inst.inreg.push regtab[a + 2];
+          else
+            num.times do |i|
+              inst.inreg.push regtab[a + 1 + i]
+            end
+          end
+
+          if Irep::OPTABLE[op] == :SENDB then
+            inst.inreg.push regtab[a + 1 + num]
+          else
+            inst.inreg.push nil
+          end
+
         else
-          p OPTABLE[op]
         end
       end
     end
-    
-    def reg_analyze
-      
+
+    def regtab
+      @root.regtab
     end
-    
+
     def inspect
       res = ""
+      res << "#{pos}  \n"
       res << "enter: "
       @enter_link.each do |ele|
         res << "#{ele.pos}, "
@@ -141,41 +318,33 @@ module RiteSSA
       @iseq.each_with_index do |ele, i|
         res << "#{Irep::disasm(ele, @irep)}\n"
       end
+      res << "exit: "
+      @exit_link.each do |ele|
+        res << "#{ele.pos}, "
+      end
+      res << "\n"
       res
     end
   end
-  
+
   class Block
     include RiteOpcodeUtil
-    
-    def collect_block_head(iseq)
-      res = [0]
-      iseq.each_with_index do |ins, pos|
-        case Irep::OPTABLE_SYM[get_opcode(ins)]
-        when :JMPIF, :JMPNOT
-          res.push (pos + getarg_sbx(ins))
-          res.push (pos + 1)
-          
-        when :JMP
-          res.push (pos + getarg_sbx(ins))
-          
-        when :RETURN
-          res.push (pos + 1)
-        end
-      end
-      
-      res.sort.uniq
-    end
-    
-    def initialize(irep)
+
+    def initialize(irep, parent, tclass)
       iseq = irep.iseq
+      @irep = irep
+      @target_class = tclass
+      @parent = parent
       @nodes = {}
-      block_head = collect_block_head(iseq)
-      block_head << iseq.size
-      block_head.each_cons(2) do |bg, ed|
-        @nodes[bg] = RiteDAGNode.new(irep, bg, iseq[bg...ed])
+      @regtab = [SelfReg.new(@irep)]
+      (@irep.nlocals - 1).times do
+        @regtab.push ParmReg.new(@irep)
       end
-      
+      block_head = collect_block_head(iseq)
+      block_head.each_cons(2) do |bg, ed|
+        @nodes[bg] = RiteDAGNode.new(irep, bg, iseq[bg...ed], self)
+      end
+
       # construct link
       @nodes.each do |beg, dag|
         lastpos = beg + dag.iseq.size - 1
@@ -184,31 +353,51 @@ module RiteSSA
         when :JMP
           @nodes[lastpos + getarg_sbx(lastins)].enter_link << dag
           dag.exit_link << @nodes[lastpos + getarg_sbx(lastins)]
-          
+
         when :JMPIF, :JMPNOT
           @nodes[lastpos + 1].enter_link << dag
           @nodes[lastpos + getarg_sbx(lastins)].enter_link << dag
           dag.exit_link << @nodes[lastpos + 1]
           dag.exit_link << @nodes[lastpos + getarg_sbx(lastins)]
-          
-          
+
         when :RETURN
-          
+
         else
-          p dag
           @nodes[lastpos + 1].enter_link << dag
           dag.exit_link << @nodes[lastpos + 1]
         end
       end
     end
-    
+
+    attr :parent
+    attr :target_class
+    attr :regtab
+
+    def collect_block_head(iseq)
+      res = [0]
+      iseq.each_with_index do |ins, pos|
+        case Irep::OPTABLE_SYM[get_opcode(ins)]
+        when :JMPIF, :JMPNOT
+          res.push (pos + getarg_sbx(ins))
+          res.push (pos + 1)
+
+        when :JMP
+          res.push (pos + getarg_sbx(ins))
+
+        when :RETURN
+          res.push (pos + 1)
+        end
+      end
+
+      res.sort.uniq
+    end
+
     def inspect
       res = ""
       @nodes.each do |pos, dag|
-        res << "#{pos}  \n"
         res << dag.inspect
       end
-      
+
       res
     end
   end
