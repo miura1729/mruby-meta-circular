caf613b7c167e2c43d9d077cd0d59d480ddac763
Miura Hideki <m-72@tf6.so-net.ne.jp>
2019-05-05 17:31:21 +0900

    Improve step0.rb

--- sample/nagoya/step0.rb
+++ sample/nagoya/step0.rb
@@ -1,26 +1,20 @@
-def foo
-  if rand < 0.5 then
-    x = 1
-  else
-    x = :a
-  end
-
+def foo(x)
   x.to_i
 end
 
-def bar
-  if rand < 0.5 then
-    x = :a
-  else
-    x = 1
-  end
-
+def bar(x)
   if !x.kind_of?(Symbol)
     x.to_i
   end
 end
 
 MTypeInf::inference_main {
-  foo
-  bar
+  if rand < 0.5 then
+    x = :a
+  else
+    x = 1
+  end
+
+  foo(x)
+  bar(x)
 }
