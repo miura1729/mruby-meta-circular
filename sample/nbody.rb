396a50e71a99c2398b21a46b4bc4122dcd73e8b6
Miura Hideki <m-72@tf6.so-net.ne.jp>
2021-09-14 11:06:23 +0900

    tune up

--- sample/nbody.rb
+++ sample/nbody.rb
@@ -9,6 +9,74 @@
 SOLAR_MASS = 4 * Math::PI*Math::PI
 DAYS_PER_YEAR = 365.24
 
+##
+# Range
+#
+# ISO 15.2.14
+class Range
+
+  ##
+  # Calls the given block for each element of +self+
+  # and pass the respective element.
+  #
+  # ISO 15.2.14.4.4
+  def each(&block)
+    return to_enum :each unless block
+
+    val = self.first
+    last = self.last
+
+    if val.kind_of?(Fixnum) && last.kind_of?(Fixnum) # fixnums are special
+      lim = last
+      lim += 1 unless exclude_end?
+      i = val
+      while i < lim
+        block.call(i)
+        i += 1
+      end
+      return self
+    end
+
+    if val.kind_of?(String) && last.kind_of?(String) # fixnums are special
+      if val.respond_to? :upto
+        return val.upto(last, exclude_end?, &block)
+      else
+        str_each = true
+      end
+    end
+
+    raise TypeError, "can't iterate" unless val.respond_to? :succ
+
+    return self if (val <=> last) > 0
+
+    while (val <=> last) < 0
+      block.call(val)
+      val = val.succ
+      if str_each
+        break if val.size > last.size
+      end
+    end
+
+    block.call(val) if !exclude_end? && (val <=> last) == 0
+    self
+  end
+
+  # redefine #hash 15.3.1.3.15
+  def hash
+    h = first.hash ^ last.hash
+    h += 1 if self.exclude_end?
+    h
+  end
+end
+
+##
+# Range is enumerable
+#
+# ISO 15.2.14.3
+class Range
+  include Enumerable
+end
+
 #def _puts *args
 #end
 
@@ -29,9 +97,10 @@ class Planet
    dy = @y - b2.y
    dz = @z - b2.z
 
-   distance = Math.sqrt(dx * dx + dy * dy + dz * dz)
-   mag = dt / (distance * distance * distance)
-   b_mass_mag, b2_mass_mag = @mass * mag, b2.mass * mag
+   distance2 = dx * dx + dy * dy + dz * dz
+   distance = Math.sqrt(distance2)
+   mag = dt / (distance2 * distance2)
+   b_mass_mag, b2_mass_mag = @mass * mag * distance, b2.mass * distance * mag
 
    @vx -= dx * b2_mass_mag
    @vy -= dy * b2_mass_mag
