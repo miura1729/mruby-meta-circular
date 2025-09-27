74e6d53185bc7d84738552c257b9dc00ccbd557b
Miura Hideki <m-72@tf6.so-net.ne.jp>
2025-09-27 11:03:27 +0900

    Add new sample for lock method

---
+++ sample/thread8.rb
@@ -0,0 +1,49 @@
+
+MTypeInf::inference_main {
+class Count
+#  include MMC_EXT::LockPolicy::NoLock
+#  include MMC_EXT::LockPolicy::LockFree
+
+  def initialize
+    @c = 1
+  end
+
+  def cnt=(v)
+    @c = v
+  end
+
+  def cnt
+    @c
+  end
+end
+
+def foo
+  i = 0
+  c = Count.new
+  r1 = [0, 0, nil]
+  r2 = [0, 0, nil]
+
+  th = MMC_EXT::Thread.new(c, r1) {|cnt, res|
+    n = 0
+    cnt.lock do |c|
+      res.lock do |r|
+        c.cnt = c.cnt + 1
+      end
+    end
+  }
+  th2 = MMC_EXT::Thread.new(c, r1) {|cnt, res|
+    n = 0
+    res.lock do |r|
+      cnt.lock do |c|
+        c.cnt = c.cnt + 1
+      end
+    end
+  }
+
+  th.join
+  th2.join
+end
+
+
+  foo
+}
