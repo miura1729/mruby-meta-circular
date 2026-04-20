ca8719b0473806c6f81b0defba966cc755aa0fae
Miura Hideki <m-72@tf6.so-net.ne.jp>
2026-04-20 17:46:40 +0900

    Bug fix for gc.c

--- gc.c
+++ gc.c
@@ -535,6 +535,10 @@ mrb_obj_alloc(mrb_state *mrb, enum mrb_vtype ttype, struct RClass *cls)
   if (gc->free_heaps == NULL) {
     add_heap(mrb, gc);
   }
+  if (gc->free_heaps->freelist == NULL) {
+    unlink_free_heap_page(gc, gc->free_heaps);
+    add_heap(mrb, gc);
+  }
 
   p = gc->free_heaps->freelist;
   gc->free_heaps->freelist = ((struct free_obj*)p)->next;
